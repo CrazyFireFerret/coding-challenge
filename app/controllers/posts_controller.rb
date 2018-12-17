60d2062312a9b91d4c99eb6c401d69f92e12d8c3
Nick Daddona <nick@canc.com>
2018-12-17 16:12:21 -0600

    Added post editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,9 @@ class PostsController < ApplicationController
           @post = Post.find(params[:id])
   end
 
+  def edit
+          @post = Post.find(params[:id])
+  end
 
 
   def create
@@ -28,6 +31,16 @@ class PostsController < ApplicationController
           end
 
   end
+
+  def update
+	  @post = Post.find(params[:id])
+	  if @post.update(post_params)
+		  redirect_to @post
+	  else
+		  render 'edit'
+	  end
+  end
+
 
   def destroy
     @post = Post.find(params[:id])
