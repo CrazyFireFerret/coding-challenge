e8cc61979bf33de26a28d5f41d69c9c7a6e66b0c
Nick Daddona <nick@canc.com>
2018-12-17 14:44:14 -0600

    Edit for comments and post times added.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,36 @@
 class CommentsController < ApplicationController
-
-  def edit
-	  @post = Post.find(params[:post_id])
-	  @comment = @post.comments.find(params[:id])
-  end
-
+  before_action :set_post
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    redirect_to @post
   end
   
-  
-  def update
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+  def edit
+          @comment = Comment.find(params[:id])
+  end
 
-    if @comment.update(comments_params)
-      redirect_to post_path(@post)
-    else
-      render 'edit'
-    end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to @post
   end
 
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post)
+    redirect_to @post
   end
   
 
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
+    end
+
+    def set_post
+	    @post = Post.find(params[:post_id])
     end
 end
